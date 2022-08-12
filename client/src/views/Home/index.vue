1df303165450b29e7ae0cb47e4f5a45bf1e1248a
Purachet Zheng <poorachad2545@gmail.com>
2022-08-12 17:11:25 +0700

    tailwind darkmode toggle

--- client/src/views/Home/index.vue
+++ client/src/views/Home/index.vue
@@ -1,13 +1,27 @@
 <script setup>
-import { useCounter } from '../../stores/counter.js'
+// import { useCounter } from '../../stores/counter.js'
+import { useDarkmode } from '../../stores/darkMode/darkMode'
+import { storeToRefs } from 'pinia'
+const myDarkmode = useDarkmode()
+const { darkmode } = storeToRefs(myDarkmode)
+myDarkmode.init()
+
+
 
 </script>
  
 <template>
-  <h1 class="text-3xl font-bold underline">
-    {{ $t("hello") }}
-  </h1>
-  
+  <div>
+    <div class="">
+      <h1 class="text-3xl font-bold underline">
+        {{ $t("hello") }}
+      </h1>
+
+      <button @click="myDarkmode.toggleMode()">toggle</button>
+
+    </div>
+  </div>
+  <h1>{{ darkmode }}</h1>
 </template>
  
 <style>
--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -4,8 +4,27 @@ module.exports = {
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '0.3': '0.1rem',
+        '15': '3.75rem',
+        '18': '4.5rem',
+        '19': '4.75rem',
+        '21': '5.25rem',
+        '98': '24.5rem',
+        '100': '25rem',
+        '128': '32rem',
+        '160': '40rem',
+        '192': '48rem',
+        '256': '64rem',
+        '288': '72rem',
+      },
+      maxWidth: {
+        '8xl': '86rem',
+      },
+    },
   },
   plugins: [],
 }
