bab9253e88dd61206a4efa2ddd2d0fbda98c4fa2
Purachet Zheng <poorachad2545@gmail.com>
2022-08-13 20:01:43 +0700

    fix animation shake when hover

--- client/src/modules/event/components/eventCard.vue
+++ client/src/modules/event/components/eventCard.vue
@@ -34,8 +34,8 @@ const event = computed(() => {
 <template>
     <li class="flex flex-col overflow-hidden shadow-md bg-white  font-semibold rounded-md cursor-pointer group"
         @click="$emit('clickEventCard')">
-        <div class="h-8 w-full bg-blue-500"></div>
-        <div class="relative bg-white p-4 flex flex-col gap-0.5 group-hover:-translate-y-8 duration-300 ease-in-out">
+        <div class="h-8 w-full bg-blue-500 mt-0 group-hover:-mt-8 duration-300 ease-in-out"></div>
+        <div class="relative bg-white p-4 flex flex-col gap-0.5">
             <span class="text-sm text-blue-500">{{ event.startDate }}</span>
             <h1 class="text-xl pb-1 font-bold">{{ event.name }}</h1>
             <h2 class="text-md text-gray-800">{{ event.category.name }}</h2>
