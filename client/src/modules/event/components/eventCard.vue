0a43f04ca6596c321d9f9c3f11b7b230dcb77519
Purachet Zheng <poorachad2545@gmail.com>
2022-08-13 22:50:15 +0700

    eventCard: show event status (Upcoming or Past)

--- client/src/modules/event/components/eventCard.vue
+++ client/src/modules/event/components/eventCard.vue
@@ -5,6 +5,7 @@ import {
     formatHourTime,
     formatFull1,
     formatFull2,
+    isFuture
 } from '../utils/dateTime'
 import { IcClock, IcClockOutline } from '../../../assets/icons/time-icons'
 import { IcArrowRight, IcArrowRightBold } from '../../../assets/icons/arrows-icons'
@@ -27,17 +28,22 @@ const event = computed(() => {
         duration: props.event.eventDuration,
         startDate: formatMonthDayYear(props.event.eventStartTime),
         startTime: formatHourTime(props.event.eventStartTime),
+        isFuture: isFuture(props.event.eventStartTime)
     }
 })
+
 </script>
 
 <template>
     <li class="flex flex-col overflow-hidden shadow-md bg-white  font-semibold rounded-md cursor-pointer group"
         @click="$emit('clickEventCard')">
-        <div class="h-8 w-full bg-blue-500 mt-0 group-hover:-mt-8 duration-300 ease-in-out"></div>
+        <div class="h-8 w-full flex items-center justify-center gap-1 text-white group-hover:-mt-8 duration-300 ease-in-out"
+            :class="[event.isFuture ? 'bg-blue-500' : 'bg-gray-400']">
+            {{ event.isFuture ? 'Upcoming' : 'Past' }}
+        </div>
         <div class="relative bg-white p-4 flex flex-col gap-0.5">
             <span class="text-sm text-blue-500">{{ event.startDate }}</span>
-            <h1 class="text-xl pb-1 font-bold">{{ event.name }}</h1>
+            <h1 class="text-xl pb-1 font-bold truncate">{{ event.name }}</h1>
             <h2 class="text-md text-gray-800">{{ event.category.name }}</h2>
             <div class="flex items-center gap-2">
                 <IcClockOutline class="text-gray-700" />
@@ -45,8 +51,9 @@ const event = computed(() => {
                     {{ event.startTime }} ({{ event.duration }} min)
                 </p>
             </div>
-            <p class="absolute w-full -bottom-8 left-0 h-8 bg-gradient-to-r from-green-400 to-blue-500 flex items-center justify-center gap-1 text-white">
-                View Detail <span class=""><IcArrowRightBold /></span>
+            <p
+                class="absolute w-full -bottom-8 left-0 h-8 flex items-center justify-center gap-1 text-white bg-gradient-to-r from-green-400 to-blue-500 ">
+                View Detail <IcArrowRightBold />
             </p>
         </div>
 
--- client/src/modules/event/utils/dateTime.js
+++ client/src/modules/event/utils/dateTime.js
@@ -3,7 +3,13 @@ import localizedFormat from 'dayjs/plugin/localizedFormat'
 dayjs.extend(localizedFormat)
 
 export const formatFull1 = (dateTime) => dayjs(dateTime).format('LLLL')
+
 export const formatFull2 = (dateTime) => dayjs(dateTime).format('llll')
+
 export const formatMonthDayYearHourTime = (dateTime) => dayjs(dateTime).format('LLL')
+
 export const formatMonthDayYear = (dateTime) => dayjs(dateTime).format('LL')
+
 export const formatHourTime = (dateTime) => dayjs(dateTime).format('LT')
+
+export const isFuture = (dateTime) => dayjs(dateTime).isAfter(dayjs())
