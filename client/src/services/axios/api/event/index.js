79d345e4c27330864bc95a0d8739bd44d0a2989e
Purachet Zheng <poorachad2545@gmail.com>
2022-08-14 17:49:19 +0700

    axios: getEventById

--- client/src/services/axios/api/event/index.js
+++ client/src/services/axios/api/event/index.js
@@ -1,9 +1,19 @@
-import apiClient from "../../apiClient";
+import apiClient from '../../apiClient'
 
 const apiEvent = {
-    get(){
-        return apiClient.get('/events');
-    }
+  get({page, pageSize, sortBy, filter, eventCategoryID, keyword} = {}) {
+    return apiClient.get('/events', { params: {
+        page: page,
+        pageSize: pageSize,
+        sortBy: sortBy,
+        filter: filter,
+        eventCategoryID: eventCategoryID,
+        keyword: keyword
+    } })
+  },
+  getById(id) {
+    return apiClient.get(`/events/${id}`)
+  },
 }
 
 export default apiEvent
