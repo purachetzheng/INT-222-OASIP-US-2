0322260c042012c2395ee408584d41ac896cc7b8
Purachet Zheng <poorachad2545@gmail.com>
2022-08-15 00:59:06 +0700

    add new validaters

---
+++ client/src/services/validater/index.js
@@ -0,0 +1,6 @@
+import validateEmail from './validateEmail'
+import validateNotes from './validateNotes'
+import validateDateTime from './validateDateTime'
+export{
+    validateEmail, validateNotes, validateDateTime
+}
