5ef4fdfeb512ba5a5a4ac287388529d258beadce
Purachet Zheng <poorachad2545@gmail.com>
2022-08-14 17:51:09 +0700

    Schedules page: filter component

--- client/src/views/event/Schedules/eventFilter.vue
+++ client/src/views/event/Schedules/eventFilter.vue
@@ -1,20 +1,32 @@
 <script setup>
+import { BaseDropdown, BaseInputDate } from '../../../components/base';
+import { IcSortRound } from '../../../assets/icons/editing-icons';
 defineEmits([])
 const props = defineProps({
-        first:{
-        type:String,
-        require:true,
-   },
+    first: {
+        type: String,
+        require: true,
+    },
 })
-
+const test = (e) => console.log(e);
 </script>
  
 <template>
-    <div>
-        
+    <div class="flex gap-2 justify-center">
+        <IcSortRound class="w-8 h-8 mr-4" />
+        <div class="w-60">
+            <BaseDropdown name="category" selected="All Category" :options="['test1', 'test2']"
+                @select-category="test" />
+        </div>
+        <div class="w-40">
+            <BaseDropdown name="event-status" selected="All Events" :options="['test1', 'test2']"
+                @select-category="test" />
+        </div>
+        <div class="w-56">
+            <BaseInputDate name="event-date" @pick-date="test" />
+        </div>
     </div>
 </template>
  
 <style>
-
 </style>
