3cdc9e3f0a39f483697bf273bcee428bb551e913
Purachet Zheng <poorachad2545@gmail.com>
2022-08-21 04:29:45 +0700

    update route

--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -1,17 +1,35 @@
 export default [
-    {
-        path: '/',
-        name: 'Home',
-        component: () => import('../views/Home/index.vue'),
-    },
-    {
-        path: '/schedules',
-        name: 'Schedules',
-        component: () => import('../views/event/Schedules/index.vue'),
-    },
-    {
-        path: '/schedules/:eventId',
-        name: 'EventDetail',
-        component: () => import('../views/event/Detail/index.vue'),
-    },
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('../views/Home/index.vue'),
+  },
+  {
+    path: '/schedules',
+    name: 'Schedules',
+    component: () => import('../views/event/Schedules/index.vue'),
+  },
+  {
+    path: '/schedules/:eventId',
+    name: 'EventDetail',
+    component: () => import('../views/event/Detail/index.vue'),
+  },
+  {
+    path: '/addEvent',
+    name: 'AddEvent',
+    component: () => import('../views/event/AddEvent/index.vue'),
+    // children: [
+    //   {
+    //     path: 'selectCategory',
+    //     name: 'SelectCategory',
+    //     component: () => import('../views/event/AddEvent/nestedViews/SelectCategory.vue'),
+    //   },
+    //   {
+    //     path: 'fillForm',
+    //     name: 'FillForm',
+    //     component: () => import('../views/event/AddEvent/nestedViews/FillForm.vue'),
+    //   },
+    // ],
+  },
+
 ]
