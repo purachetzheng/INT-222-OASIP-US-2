cd24d53abf6d76f17e1cd808153dc2c704b273ed
Purachet Zheng <poorachad2545@gmail.com>
2022-08-21 04:26:32 +0700

    add new api

--- client/src/services/validation/index.js
+++
@@ -1,6 +0,0 @@
-import validateEmail from './validateEmail'
-import validateNotes from './validateNotes'
-import validateDateTime from './validateDateTime'
-export{
-    validateEmail, validateNotes, validateDateTime
-}
