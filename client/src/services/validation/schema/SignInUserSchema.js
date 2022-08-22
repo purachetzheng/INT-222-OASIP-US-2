b103396bd8b403ea0c003eb09102aa6b11bcdc12
Purachet Zheng <poorachad2545@gmail.com>
2022-08-23 06:48:39 +0700

    add SignIn validate schema

---
+++ client/src/services/validation/schema/SignInUserSchema.js
@@ -0,0 +1,9 @@
+import emailRules from '../variable-rules/event/email-rules'
+import nameRules from '../variable-rules/event/name-rules'
+
+const schema = {
+  name: nameRules,
+  email: emailRules,
+}
+
+export default schema
