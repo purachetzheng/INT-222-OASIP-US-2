413d55127b45ca5f741d172314ffabed56e799ba
Purachet Zheng <poorachad2545@gmail.com>
2022-08-23 05:58:14 +0700

    add icons

--- client/src/assets/fontawesome/free-regular.js
+++ client/src/assets/fontawesome/free-regular.js
@@ -2,12 +2,14 @@ import {
     faClock, faCalendar,
     faMoon, 
     faCircle,
+    faTrashCan, faPenToSquare, faUser,
 } from '@fortawesome/free-regular-svg-icons'
 
 const regularIconsList = [
     faClock, faCalendar,
     faMoon,
     faCircle,
+    faTrashCan, faPenToSquare, faUser,
 ]
 
 export default regularIconsList
--- client/src/assets/fontawesome/free-solid.js
+++ client/src/assets/fontawesome/free-solid.js
@@ -3,13 +3,15 @@ import
 { 
     faCoffee, faUserSecret, faSpinner, faCircle, faTimes, faTag, 
     faArrowRight, 
-    faPlus, faPlusCircle,
+    faPlus, faPlusCircle, 
+    faUser,
 } from '@fortawesome/free-solid-svg-icons'
 
 const solidIconsList = [
     faCoffee, faUserSecret, faSpinner, faCircle, faTimes, faTag, 
     faArrowRight,
     faPlus,faPlusCircle,
+    faUser,
 ]
 
 export default solidIconsList
