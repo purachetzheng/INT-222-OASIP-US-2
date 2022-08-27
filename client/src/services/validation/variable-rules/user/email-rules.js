6190cb9dc6d0a6a16da0e67c83217b532534a6da
Purachet Zheng <poorachad2545@gmail.com>
2022-08-28 02:57:56 +0700

    refactor

--- client/src/services/validation/variable-rules/user/email-rules.js
+++ client/src/services/validation/variable-rules/user/email-rules.js
@@ -7,9 +7,10 @@ const field = {
 
 const email = (value) => {
   if (isBlank(value)) return `${label} is a required field`
+  value = value.trim()
   // if (!isTrimmed(value)) return `${label} must be a trimmed string`
   if (!isEmail(value)) return `${label} must be a valid email`
-  if (max(value.trim(), field.max)) return `${label} must be at most ${field.max} characters`
+  if (max(value, field.max)) return `${label} must be at most ${field.max} characters`
   return true
 }
 export default email
