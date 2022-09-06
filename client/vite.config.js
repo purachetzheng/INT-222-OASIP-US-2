4d16da0e260a7376a454f746bcfa4cba60086d6e
Purachet Zheng <poorachad2545@gmail.com>
2022-09-07 01:49:02 +0700

    env config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,24 +1,46 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/us2/',
-  
-  resolve:{
-    alias:{
-      '@' : path.resolve(__dirname, './src')
+
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+  return defineConfig({
+    plugins: [vue()],
+    base: '/us2/',
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
-  plugins: [
-    vue(),
-  ],
-  server: {
-    proxy: {
-      '/api': {
-        target: "http://localhost:8080",
-        changeOrigin: true
-      }
-    }
-  }
+    server: {
+      proxy: {
+        '/api': {
+          target: process.env.VITE_BASE_URL,
+          changeOrigin: true,
+        },
+      },
+    },
+  })
 })
+// export default defineConfig({
+//   base: '/us2/',
+
+//   resolve:{
+//     alias:{
+//       '@' : path.resolve(__dirname, './src')
+//     },
+//   },
+//   plugins: [
+//     vue(),
+//   ],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         // target: "http://localhost:8080",
+//         target: process.env.VITE_BASE_URL,
+//         changeOrigin: true
+//       }
+//     }
+//   }
+// })
