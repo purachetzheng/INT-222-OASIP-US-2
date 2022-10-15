d5f41fb7fbfcd1499349f8b6c9c7bdad13eaf5d0
Purachet Zheng <poorachad2545@gmail.com>
2022-10-16 01:08:44 +0700

    wrapper: remove old nav

--- client/src/components/Layout/PageWrapper.vue
+++ client/src/components/Layout/PageWrapper.vue
@@ -1,25 +1,29 @@
 <script setup>
-import SideBar from './sidebar/SideBar.vue';
-
+import SideBar from './sidebar/SideBar.vue'
+import NavBar from '../../layouts/TheNavBar/index.vue';
 </script>
 
 <template>
-  <div class="app-container flex">
-      <SideBar />
+  <div class="app-wrapper">
+    <SideBar />
+    <div class="app-container flex">
+      <!-- <NavBar /> -->
       <div class="app-content">
         <slot />
       </div>
     </div>
+  </div>
 </template>
 
 <style scoped>
-.app-wrapper{
-    @apply min-h-screen w-full flex flex-col bg-gradient-to-b from-slate-300 to-slate-100 dark:bg-gray-700
+
+.app-wrapper {
+  @apply min-h-screen w-full flex bg-gradient-to-b from-slate-300 to-slate-100 dark:bg-gray-700;
 }
-.app-container{
-    @apply w-full mx-auto  xl:px-0 h-full
+.app-container {
+  @apply w-full mx-auto xl:px-0 h-full flex flex-col;
 }
-.app-content{
-    @apply py-8 px-6 h-full 
+.app-content {
+  @apply py-8 px-6 h-full overflow-y-auto;
 }
 </style>
