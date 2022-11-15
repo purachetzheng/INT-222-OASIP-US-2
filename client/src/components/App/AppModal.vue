627ce4d33b15b9db74b1fbe1469408be5c914d99
Purachet Zheng <poorachad2545@gmail.com>
2022-11-16 04:26:30 +0700

    AppModal: update

--- client/src/components/App/AppModal.vue
+++ client/src/components/App/AppModal.vue
@@ -1,76 +1,80 @@
 <script setup>
 const emits = defineEmits(['close'])
 const props = defineProps({
-  show: {
-    type: Boolean,
-    default: false,
-  },
-  type: {
-    type: String,
-    default: 'blank',
-  },
+    show: {
+        type: Boolean,
+        default: false,
+    },
+    type: {
+        type: String,
+        default: 'blank',
+    },
 })
 </script>
 
 <template>
-  <Teleport to="body">
-    <Transition name="modal">
-      <div v-if="show" class="modal-mask">
-        <div
-          class="modal-container p-8 bg-gray-50 rounded-md flex flex-col gap-4"
-        >
-          <div class="modal-header">
-            <slot name="header">default header</slot>
-          </div>
+    <Teleport to="body">
+        <Transition name="modal">
+            <div v-if="show" class="modal-mask">
+                <div
+                    class="modal-container p-8 bg-gray-50 rounded-md flex flex-col gap-4"
+                >
+                    <div class="modal-header">
+                        <slot name="header">default header</slot>
+                    </div>
 
-          <div class="modal-body">
-            <slot name="body">default body</slot>
-          </div>
+                    <div class="modal-body">
+                        <slot name="body">default body</slot>
+                    </div>
 
-          <div class="modal-footer" v-if="type === 'info'">
+                    <slot name="footer">
+                        <div class="modal-footer" v-if="type === 'info'"></div>
+                        <div
+                            class="modal-footer"
+                            v-if="type === 'confirm'"
+                        >
+                            <div class="flex justify-center gap-2">
+                                <app-button btn-type="outline-danger">No</app-button>
+                                <app-button btn-type="success">Yes</app-button>
+                            </div>
+                        </div>
 
-          </div>
-          <div class="modal-footer" v-if="type === 'confirm'">
-            
-          </div>
-
-          <div class="modal-footer" v-else >
-            <slot name="footer">
-              <div class="flex justify-center">
-                <app-button button-type="success">Ok</app-button>
-              </div>
-            </slot>
-          </div>
-        </div>
-      </div>
-    </Transition>
-  </Teleport>
+                        <div class="modal-footer" v-else>
+                            <div class="flex justify-center">
+                                <app-button btn-type="success">Ok</app-button>
+                            </div>
+                        </div>
+                    </slot>
+                </div>
+            </div>
+        </Transition>
+    </Teleport>
 </template>
 
 <style scoped>
 .modal-mask {
-  position: fixed;
-  z-index: 9998;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  transition: opacity 0.3s ease;
+    position: fixed;
+    z-index: 9998;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: opacity 0.3s ease;
 }
 
 .modal-container {
-  /* width: 300px; */
-  /* width: 100%; */
-  /* margin: 0px auto; */
-  /* padding: 20px 30px; */
-  background-color: #fff;
-  /* border-radius: 2px; */
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
-  transition: all 0.3s ease;
+    /* width: 300px; */
+    /* width: 100%; */
+    /* margin: 0px auto; */
+    /* padding: 20px 30px; */
+    background-color: #fff;
+    /* border-radius: 2px; */
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+    transition: all 0.3s ease;
 }
 
 /* .modal-header h3 {
@@ -96,16 +100,16 @@ const props = defineProps({
  */
 
 .modal-enter-from {
-  opacity: 0;
+    opacity: 0;
 }
 
 .modal-leave-to {
-  opacity: 0;
+    opacity: 0;
 }
 
 .modal-enter-from .modal-container,
 .modal-leave-to .modal-container {
-  -webkit-transform: scale(1.1);
-  transform: scale(1.1);
+    -webkit-transform: scale(1.1);
+    transform: scale(1.1);
 }
 </style>
