5a76ec4626d6e043b77725283679f04b75fcb307
Purachet Zheng <poorachad2545@gmail.com>
2022-11-20 02:53:03 +0700

    refactor

--- client/src/views/eventCategory/eventCategoryList/components/EditCategoryModal.vue
+++ client/src/views/eventCategory/eventCategoryList/components/EditCategoryModal.vue
@@ -9,37 +9,37 @@ import RoleSelectField from '../../../../components/user/RoleSelectField.vue'
 
 const emits = defineEmits(['close', 'submit-edit-form'])
 const props = defineProps({
-  modalState: {
-    type: Object,
-    default: {
-      // category:{}
+    modalState: {
+        type: Object,
+        default: {
+            // category:{}
+        },
     },
-  },
-  // editingCategory:{
-  //   type: Object,
-  //   default: {},
-  // }
+    // editingCategory:{
+    //   type: Object,
+    //   default: {},
+    // }
 })
 
 const formConfig = ref({
-  isLoading: true,
-  name: {
-    maxLength: 100,
-  },
-  duration: {
-    min: 1,
-    max: 480,
-  },
-  description: {
-    maxLength: 500,
-  },
+    isLoading: true,
+    name: {
+        maxLength: 100,
+    },
+    duration: {
+        min: 1,
+        max: 480,
+    },
+    description: {
+        maxLength: 500,
+    },
 })
 
 const formTemplate = {
-  id: null,
-  name: '',
-  duration: 1,
-  description: '',
+    id: null,
+    name: '',
+    duration: 1,
+    description: '',
 }
 
 // const formTemplate = {
@@ -49,10 +49,10 @@ const formTemplate = {
 // }
 
 const { handleSubmit, values, resetForm, meta, setFieldValue, setValues } =
-  useForm({
-    validationSchema: schema,
-    initialValues: { ...formTemplate },
-  })
+    useForm({
+        validationSchema: schema,
+        initialValues: { ...formTemplate },
+    })
 // const onSubmit = handleSubmit(({name = '', email = '', role, password, confirmPassword}) => {
 //   const isPasswordConfirm = password === confirmPassword
 //   if (!isPasswordConfirm)
@@ -67,71 +67,73 @@ const { handleSubmit, values, resetForm, meta, setFieldValue, setValues } =
 //   emits('submit-form', trimmedUser)
 // })
 
-const onSubmit = handleSubmit(({id , name, duration, description}) => {
-  // console.log(values);
-  const category = {
-    id,
-    eventCategoryName: name.trim(),
-    eventDuration: duration,
-    eventCategoryDescription: !description.trim() ?  null : description.trim()
-  }
-  emits('submit-edit-form', category)
+const onSubmit = handleSubmit(({ id, name, duration, description }) => {
+    // console.log(values);
+    const category = {
+        id,
+        eventCategoryName: name.trim(),
+        eventDuration: duration,
+        eventCategoryDescription: !description.trim()
+            ? null
+            : description.trim(),
+    }
+    emits('submit-edit-form', category)
 })
 onUpdated(async () => {
-  if (props.modalState.visible) {
-    const category = props.modalState.category
-    setValues({
-      id: category.id,
-      name: category.eventCategoryName,
-      duration: category.eventDuration,
-      description: category.eventCategoryDescription || '',
-    })
-    return
-  }
+    if (props.modalState.visible) {
+        const category = props.modalState.category
+        setValues({
+            id: category.id,
+            name: category.eventCategoryName,
+            duration: category.eventDuration,
+            description: category.eventCategoryDescription || '',
+        })
+        return
+    }
 })
 </script>
 
 <template>
-  <app-modal :show="modalState.visible">
-    <template #header>
-      <h1 class="text-2xl font-semibold text-center">Edit Category</h1>
-      <!-- {{editingCategory}} -->
-    </template>
-    <template #body>
-      <div class="flex flex-col gap-0 w-128">
-        <InputField
-          id="name"
-          class=""
-          name="name"
-          type="text"
-          :max="formConfig.name.maxLength"
-          label="Category Name"
-          :required="true"
-        />
-        <InputField
-          id="duration"
-          class=""
-          name="duration"
-          type="number"
-          label="Duration"
-          :required="true"
-        />
-        <app-vee-textarea
-          name="description"
-          :max="formConfig.description.maxLength"
-          label="Description"
-        />
-      </div>
-    </template>
-    <template #footer>
-      <div class="flex gap-4">
-        <app-button btn-type="success" @click="onSubmit">Submit</app-button>
-        <app-button btn-type="outline-danger" @click="modalState.close"
-          >Cancel</app-button
-        >
-      </div>
-    </template>
-  </app-modal>
+    <app-modal :show="modalState.visible">
+        <template #header>
+            <h1 class="text-2xl font-semibold text-center">Edit Category</h1>
+            <!-- {{editingCategory}} -->
+        </template>
+        <template #body>
+            <div class="flex flex-col gap-0 w-128">
+                <app-vee-input
+                    name="name"
+                    type="text"
+                    :max="formConfig.name.maxLength"
+                    label="Name"
+                    required
+                    counter
+                />
+                <app-vee-input
+                    name="duration"
+                    type="number"
+                    :max="formConfig.name.maxLength"
+                    label="Duration"
+                    required
+                />
+                <app-vee-textarea
+                    name="description"
+                    :max="formConfig.description.maxLength"
+                    label="Description"
+                />
+            </div>
+        </template>
+        <template #footer>
+            <div class="flex gap-4">
+                <app-button btn-type="success" @click="onSubmit"
+                    >Submit</app-button
+                >
+                <app-button btn-type="secondary" @click="modalState.close"
+                    >Cancel</app-button
+                >
+            </div>
+        </template>
+    </app-modal>
 </template>
 
 <style scoped></style>
--- client/src/views/user/Users/index.vue
+++ client/src/views/user/Users/index.vue
@@ -3,127 +3,138 @@ import { computed, onBeforeMount, provide, readonly, ref } from 'vue'
 import { apiUser } from '../../../services/api/lib'
 import UserTable from './UserTable.vue'
 import UserDetailModal from './modal/UserDetailModal.vue'
-import AddUserModal from './modal/AddUserModal.vue';
-import PageWrapper from '../../../components/Layout/PageWrapper.vue';
+import AddUserModal from './modal/AddUserModal.vue'
+import PageWrapper from '../../../components/Layout/PageWrapper.vue'
 const users = ref([])
 
 const getUsers = async (page) => {
-  try {
-    const { data, status } = await apiUser.get()
-    // const { content, number, totalPages } = data
-    users.value = data
-    // return data.content
-  } catch (error) {
-    console.log(error)
-    // console.log(error.response)
-    // const res = error.response
-    // console.log(res.status)
-    // console.log('error ', error.message)
-  }
+    try {
+        const { data, status } = await apiUser.get()
+        // const { content, number, totalPages } = data
+        users.value = data
+        // return data.content
+    } catch (error) {
+        console.log(error)
+        // console.log(error.response)
+        // const res = error.response
+        // console.log(res.status)
+        // console.log('error ', error.message)
+    }
 }
 
 const askForDelete = (id) => {
-  if(confirm('Do you really want to delete?')) deleteUsers(id)
+    if (confirm('Do you really want to delete?')) deleteUsers(id)
 }
 const deleteUsers = async (id) => {
-  try {
-    const { data, status } = await apiUser.delete(id)
-    // const { content, number, totalPages } = data
-    users.value = users.value.filter((u) => u.id != id)
-    alert(`successfully deleted`)
-    
-    // return data.content
-  } catch (error) {
-    // console.log(error)
-    // console.log(error.response)
-    const res = error.response
-    console.log(res.status)
-    console.log('error ', error.message)
-  }
+    try {
+        const { data, status } = await apiUser.delete(id)
+        // const { content, number, totalPages } = data
+        users.value = users.value.filter((u) => u.id != id)
+        alert(`successfully deleted`)
+
+        // return data.content
+    } catch (error) {
+        // console.log(error)
+        // console.log(error.response)
+        const res = error.response
+        console.log(res.status)
+        console.log('error ', error.message)
+    }
 }
 
 const updateUsers = async (id, modifyUser, setFieldError) => {
-  try {
-    const { data } = await apiUser.patch(id, modifyUser)
-    users.value = users.value.map((user) => user.id === data.id ? data : user)
-    selectedUser.value = data
-    alert('ok')
-  } catch (error) {
-    const { data, status } = error.response
-    const { details } = data
-    if(!details) return alert(status + ' error')
-    details.forEach((details) => {
-      setFieldError(details.field, details.errorMessage)
-    })
-  }
+    try {
+        const { data } = await apiUser.patch(id, modifyUser)
+        users.value = users.value.map((user) =>
+            user.id === data.id ? data : user
+        )
+        selectedUser.value = data
+        alert('ok')
+    } catch (error) {
+        const { data, status } = error.response
+        const { details } = data
+        if (!details) return alert(status + ' error')
+        details.forEach((details) => {
+            setFieldError(details.field, details.errorMessage)
+        })
+    }
 }
 
 const onSubmit = async (user) => {
-  try {
-    const { data } = await apiUser.post(user)
-    console.log(data)
-    users.value.push(data)
-    closeAddUserModal()
-    alert('ok')
-  } catch (error) {
-    console.log(error)
-    const { data, status } = error.response
-    const { details } = data
-    let messageError = ''
-    details.forEach(details => {
-        messageError+=`${details.field}: ${details.errorMessage}\n`
-    });
-    alert(messageError)
-  }
+    try {
+        const { data } = await apiUser.post(user)
+        console.log(data)
+        users.value.push(data)
+        closeAddUserModal()
+        alert('ok')
+    } catch (error) {
+        console.log(error)
+        const { data, status } = error.response
+        const { details } = data
+        let messageError = ''
+        details.forEach((details) => {
+            messageError += `${details.field}: ${details.errorMessage}\n`
+        })
+        alert(messageError)
+    }
 }
 
 const [selectedUser, selectUser, resetSelectUser, showDetailModal] = [
-  ref({}),
-  (user) => (selectedUser.value = user),
-  () => (selectedUser.value = {}),
-  computed(() => Object.keys(selectedUser.value).length !== 0),
+    ref({}),
+    (user) => (selectedUser.value = user),
+    () => (selectedUser.value = {}),
+    computed(() => Object.keys(selectedUser.value).length !== 0),
 ]
 
 const [showAddUserModal, openAddUserModal, closeAddUserModal] = [
-  ref(false),
-  () => (showAddUserModal.value = true),
-  () => (showAddUserModal.value = false),
+    ref(false),
+    () => (showAddUserModal.value = true),
+    () => (showAddUserModal.value = false),
 ]
 
 onBeforeMount(() => {
-  getUsers()
+    getUsers()
 })
 </script>
 
 <template>
-  <PageWrapper>
-  <main class="flex flex-col  gap-0 justify-between">
-    <header class="flex justify-between">
-      <h1 class="text-2xl font-bold">User</h1>
-      <div class="flex items-center gap-6">
-        <fa-icon :icon="['fas', 'ellipsis']" class="fa-xl" />
-        <button class="p-2 bg-blue-500" @click="openAddUserModal">Add User</button>
-      </div>
-    </header>
+    <PageWrapper>
+        <main class="flex flex-col gap-0 justify-between">
+            <header class="flex justify-between">
+                <h1 class="text-2xl font-bold">User</h1>
+                <div class="flex items-center gap-6">
+                    <fa-icon :icon="['fas', 'ellipsis']" class="fa-xl" />
+                    <app-button
+                        btn-type="primary"
+                        btn-size="md"
+                        @click="openAddUserModal"
+                        >Add User</app-button
+                    >
+                </div>
+            </header>
 
-    <UserTable
-      :users="users"
-      @delete-user="askForDelete"
-      @view-detail="selectUser"
-      class="grow overflow-hidden"
-    />
+            <UserTable
+                :users="users"
+                @delete-user="askForDelete"
+                @view-detail="selectUser"
+                class="grow overflow-hidden"
+            />
 
-    <div class="">Foot</div>
+            <div class="">Foot</div>
 
-    <UserDetailModal
-      :show="showDetailModal"
-      :user="selectedUser"
-      @update-user="updateUsers"
-      @close="resetSelectUser"
-    />
-    <AddUserModal :show="showAddUserModal" @close="closeAddUserModal" @submit-form="onSubmit" />
-  </main>
-</PageWrapper>
+            <UserDetailModal
+                :show="showDetailModal"
+                :user="selectedUser"
+                @update-user="updateUsers"
+                @close="resetSelectUser"
+            />
+            <AddUserModal
+                :show="showAddUserModal"
+                @close="closeAddUserModal"
+                @submit-form="onSubmit"
+            />
+        </main>
+    </PageWrapper>
 </template>
 
 <style></style>
