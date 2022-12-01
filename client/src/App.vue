ebba22a1802068058ddf642b82304e99ecf87ca9
Purachet Zheng <poorachad2545@gmail.com>
2022-12-02 00:25:55 +0700

    delete commend

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,10 +7,7 @@ const userStore = useUserStore()
 // userStore.getUserInfo()
 userStore.init()
 appStore.init()
-// appStore.expandSidebar()
-console.log(appStore.sidebarCollapsed);
-// console.log(Boolean(appStore.getSidebarCollapsed()));
-// console.log();
+
 </script>
  
 <template>
