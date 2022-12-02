396f50aad48be8264b7d1c38bce487d1364fbbc2
Purachet Zheng <poorachad2545@gmail.com>
2022-12-03 02:45:51 +0700

    DevTest: add font element

--- client/src/views/DevTest/DevTest.vue
+++ client/src/views/DevTest/DevTest.vue
@@ -6,7 +6,28 @@ import PageWrapper from '../../components/Layout/PageWrapper.vue'
 
 <template>
   <PageWrapper>
-    <div class="">
+    <div class="flex flex-col gap-4">
+        <p class="">font</p>
+        <div class="flex gap-4">
+            <p class="font-thin">md-thin</p>
+            <p class="font-extralight">md-extralight</p>
+            <p class="font-light">md-light</p>
+            <p class="font-normal">md-normal</p>
+            <p class="font-medium">md-medium</p>
+            <p class="font-semibold">md-semibold</p>
+            <p class="font-bold">md-bold</p>
+            <p class="font-extrabold">md-extrabold</p>
+        </div>
+        <div class="flex gap-4">
+            <p class="text-lg font-thin">lg-thin</p>
+            <p class="text-lg font-extralight">lg-extralight</p>
+            <p class="text-lg font-light">lg-light</p>
+            <p class="text-lg font-normal">lg-normal</p>
+            <p class="text-lg font-medium">lg-medium</p>
+            <p class="text-lg font-semibold">lg-semibold</p>
+            <p class="text-lg font-bold">lg-bold</p>
+            <p class="text-lg font-extrabold">lg-extrabold</p>
+        </div>
         <p class="">App Form</p>
         <div class="flex flex-col w-80">
             <app-vee-input type="text" name="text" />
@@ -24,7 +45,7 @@ import PageWrapper from '../../components/Layout/PageWrapper.vue'
             <app-button btn-size="lg">Button SM</app-button>
             <app-button btn-size="xl">Button SM</app-button>
         </div>
-        <div class="mt-4 flex items-end gap-2">
+        <div class="flex items-end gap-2">
             <app-button btn-size="lg" btn-type="primary">Primary</app-button>
             <app-button btn-size="lg" btn-type="secondary">Secondary</app-button>
             <app-button btn-size="lg" btn-type="success">Success</app-button>
@@ -32,7 +53,7 @@ import PageWrapper from '../../components/Layout/PageWrapper.vue'
             <app-button btn-size="lg" btn-type="warning">Warning</app-button>
             <app-button btn-size="lg" btn-type="muted">Muted</app-button>
         </div>
-        <div class="mt-4 flex items-end gap-2">
+        <div class="flex items-end gap-2">
             <app-button btn-size="lg" btn-type="outline-primary">Outline Primary</app-button>
             <app-button btn-size="lg" btn-type="outline-secondary">Outline Secondary</app-button>
             <app-button btn-size="lg" btn-type="outline-success">Outline Success</app-button>
@@ -40,6 +61,8 @@ import PageWrapper from '../../components/Layout/PageWrapper.vue'
             <app-button btn-size="lg" btn-type="outline-warning">Outline Warning</app-button>
             <app-button btn-size="lg" btn-type="outline-muted">Outline Muted</app-button>
         </div>
+
+        
     </div>
   </PageWrapper>
 </template>
