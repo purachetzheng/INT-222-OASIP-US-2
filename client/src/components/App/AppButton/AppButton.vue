0e1f562bb707fe238764c9b5181a77770c612808
Purachet Zheng <poorachad2545@gmail.com>
2022-12-12 05:15:00 +0700

    AppButton: added new types, animations and outline settings

---
+++ client/src/components/App/AppButton/AppButton.vue
@@ -0,0 +1,80 @@
+<script setup>
+defineEmits([])
+const props = defineProps({
+  btnType: {
+    type: String,
+    default: 'primary',
+  },
+  btnIcon: {
+    type: Array,
+    default: null
+  },
+  btnSize: {
+    type: String,
+    default: 'md',
+    validator(value){
+      return ['sm','md','lg','xl'].includes(value)
+    }
+  },
+  outline: Boolean
+})
+</script>
+
+<template>
+  <button class="" :class="[`btn ${btnType} btn-${btnSize}`, props.outline && 'btn-outline']">
+    <fa-icon :icon="props.btnIcon" v-if="props.btnIcon !== null" class="pr-2" />
+    <slot></slot>
+    <div v-if="0" class="px-4 py-2 bg-indigo-50 outline-none border border-indigo-100 rounded text-indigo-500 font-medium 
+    active:scale-95 
+    hover:bg-indigo-400 hover:text-white 
+    focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 
+    disabled:bg-gray-400/80 disabled:shadow-none disabled:cursor-not-allowed 
+    transition-colors duration-200"></div>
+  </button>
+</template>
+
+<style scoped>
+@import url('buttonColor.css');
+
+.btn {
+  /* @apply border-2 rounded-md box-border font-medium select-none duration-300 items-center; */
+  @apply px-4 py-2 outline-none rounded-md text-white font-medium duration-300 transition-colors items-center;
+}
+.btn:enabled:active{
+  @apply scale-95
+}
+.btn:disabled {
+  /* @apply text-white bg-gray-400 border-gray-400; */
+  @apply bg-gray-400/80 shadow-none cursor-not-allowed;
+}
+/* .btn:focus{
+  @apply bg-indigo-600 ring-2 ring-indigo-600 ring-offset-2
+} */
+.btn-outline{
+  @apply py-1.5 border-2 bg-transparent !important;
+}
+.btn-outline:hover{
+  @apply border-transparent !important;
+}
+/* [class*="outline"]{
+  padding-top: 0.4375rem;
+  padding-bottom: 0.4375rem;
+} */
+/* .btn-sm{
+  @apply px-2.5 h-8 text-sm
+}
+.btn-md{
+  @apply px-3.5 h-9 text-sm
+}
+.btn-lg{
+  @apply px-4 h-10
+}
+.btn-xl{
+  @apply px-5 h-12 text-lg
+} */
+
+
+.no-border {
+  @apply border-transparent
+}
+</style>
